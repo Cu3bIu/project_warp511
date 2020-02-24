d6dbbb5b86065b1af607de392c6d0e9d0804c95d
serjp <baaastard@gmail.com>
2020-02-24 23:43:24 +0200

    Rebuilt Projectile with Sprites Missiles changing angle to align with movement vector

--- my_game.py
+++ my_game.py
@@ -1,34 +1,36 @@
 import pygame
 import sys
-import time
 
 from entity_classes import Entity
-from projectile_classes import Projectile
+from projectile_classes import Projectile, Enemy
 
-# setting the size of the screen
-screen_x = 1200
-screen_y = 700
-screen = pygame.display.set_mode((screen_x, screen_y))
 
-# setting the game name
+pygame.init()
+clock = pygame.time.Clock()
+
+# Setting the size of the screen
+width, height = (1200, 700)
+screen = pygame.display.set_mode((width, height))
+fps = 30
+
+# Setting the game name
 pygame.display.set_caption('my first game')
 
+# Colors
 red = (255, 0, 0)
 black = (0, 0, 0)
 
 elements_list = []
 move_list = []
-bullets = pygame.sprite.Group()    # Group for bullets on screen
+all_sprites = pygame.sprite.Group()
+bullets = pygame.sprite.Group()
 
 mass = 20
-
-player = Entity(300, 150, 30, 30, mass, 10, (255, 0, 0), screen, 'res/ship/player100x100.png')
-player.draw()
-
-pygame.display.update()
-
-# adding elements that are currently active
+player = Entity(width/2, height/2, 30, 30, mass, 10, (255, 0, 0), screen, 'res/ship/player100x100.png')
 elements_list.append(player)
+
+enemy = Enemy(width/2, 100)
+enemy.add(all_sprites)
 
 background_image = pygame.image.load('res/background/star-sky.png').convert()
 
@@ -39,36 +41,44 @@ def add_to_move_list(x):
     except ValueError:
         move_list.append(x)
 
-
+# Game loop
 while True:
+    clock.tick(fps)
+
     screen.blit(background_image, [0, 0])
 
+    # Input handling
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+        # Move player
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             print("----click----")
             mx, my = pygame.mouse.get_pos()
             player.move_to(mx, my)
             add_to_move_list(player)
 
-        # click on right mouse button - fire
+        # Fire
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
             print("----click2----")
             projectile = Projectile(player.x, player.y, pygame.mouse.get_pos())
             print(projectile.angle)
-            bullets.add(projectile)
+            projectile.add(bullets, all_sprites)
 
-    # move all bullets for one step
+    # Moving and updating
+    all_sprites.update()
     bullets.update()
+
+    # Removing bullets those are out of screen
     for bullet in bullets:
         print(bullet.pos, bullet.target)
         if not screen.get_rect().contains(bullet.rect):
-            bullet.remove(bullets)
+            bullet.remove(bullets, all_sprites)
 
-    # draw all bullets
-    bullets.draw(screen)
+    # Drawing
+    all_sprites.draw(screen)
 
     for element in move_list:
         if element.need_to_move():
@@ -80,4 +90,5 @@ while True:
     # print(len(move_list))
     pygame.display.update()
     print('------------------NEXT TICK-----------------')
-    time.sleep(0.1)
+
+pygame.quit()
